8175f953b54a4e7ec73c7d7001c4088e2170e240
devhoss <velasco2288@gmail.com>
2020-09-26 08:59:09 -0500

    add jwt.

--- src/main/java/com/devhoss/TipoCambioApplication.java
+++ src/main/java/com/devhoss/TipoCambioApplication.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import com.devhoss.model.TipoCambio;
+import com.devhoss.security.JWTAuthorizationFilter;
 import com.devhoss.service.ITipoCambioService;
+import org.springframework.http.HttpMethod;
+
 
 @SpringBootApplication
 public class TipoCambioApplication {
@@ -30,6 +38,20 @@ public class TipoCambioApplication {
 
 			TipoCambio dolaresSoles = new TipoCambio(2L, "$", "S", 3.40, 1.0);
 			iTipoCambioService.Save(dolaresSoles);
+		}
+	}
+	
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+				.anyRequest().authenticated();
 		}
 	}
 
